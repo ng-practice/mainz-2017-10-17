fb3c936914618638187b75faca2348c465ab73f3
Gregor Woiwode <gw@co-it.eu>
2017-09-26 10:43:47 +0200

    (exercise:http-client) load books from API

--- src/app/book/book.module.ts
+++ src/app/book/book.module.ts
@@ -1,17 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+
 import { InfoBoxComponent } from './info-box/info-box.component';
 import { BookList } from './book-list/book-list.component';
 import { BookCard } from './book-card/book-row.component';
-import {BookShelf} from './services/book-shelf.service';
-
+import { BookShelf } from './services/book-shelf.service';
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
+  imports: [CommonModule, HttpClientModule],
   declarations: [InfoBoxComponent, BookList, BookCard],
   exports: [InfoBoxComponent, BookList],
   providers: [BookShelf]
 })
-export class BookModule { }
+export class BookModule {}
