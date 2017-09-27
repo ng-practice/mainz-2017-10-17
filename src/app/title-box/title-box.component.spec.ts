96ca9ecb542f58c8a5e44409421c3c3c62fa6809
Gregor Woiwode <gw@co-it.eu>
2017-09-27 11:01:20 +0200

    (component:title-box) provide shallow test

--- src/app/title-box/title-box.component.spec.ts
+++ src/app/title-box/title-box.component.spec.ts
@@ -1,6 +1,9 @@
-// Test Suite
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+
 import { TitleBox } from './title-box.component';
 
+// Test Suite
 describe('[Unit] Component TitleBox', () => {
   describe('When title is clicked', () => {
     let titleBox: TitleBox;
@@ -18,6 +21,39 @@ describe('[Unit] Component TitleBox', () => {
 
       // assertion
       expect(titleClicked).toHaveBeenCalled();
+    });
+  });
+});
+
+describe('[Shallow] Component TitleBox', () => {
+  let fixture: ComponentFixture<TitleBox>;
+  let titleBox: TitleBox;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [TitleBox]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(TitleBox);
+    titleBox = fixture.componentInstance;
+  });
+
+  describe('When the title is clicked', () => {
+    let titleClicked: jasmine.Spy;
+
+    beforeEach(() => {
+      titleClicked = spyOn(titleBox.titleClicked, 'emit');
+
+      // query template for h3
+      const title = fixture.debugElement.query(By.css('h3'));
+      (title.nativeElement as HTMLTitleElement).click();
+
+      // trigger change detection
+      fixture.detectChanges();
+    });
+
+    it('the event "titleClicked" is raised', () => {
+      expect(titleClicked).toHaveBeenCalledWith('Hey there! I wish you a pleasent and sunny day.');
     });
   });
 });
