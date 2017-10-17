3ed23fa96920fc54c25dd8baf8b89137c61b3c85
Gregor Woiwode <gregor.woiwode@gmail.com>
2017-10-17 12:05:21 +0200

    (lab) start implementing injection helper "use"

--- src/app/book/services/book-shelf.service.spec.ts
+++ src/app/book/services/book-shelf.service.spec.ts
@@ -31,21 +31,38 @@ describe('[HttpMock] Service:BookShelf', () => {
   });
 
   describe('When an error occurs while loading books', () => {
+    // Without this type TypeScript-Compiler complains.
+    type Deps = [typeof BookShelf, typeof HttpTestingController];
+
+    const deps: Deps = [BookShelf, HttpTestingController];
+
     it('the error message "Could not load books" should be provided',
-      inject(
-        [BookShelf, HttpTestingController],
-        fakeAsync((service: BookShelf, httpMock: HttpTestingController) => {
-          service
-            .all()
-            .subscribe(
-              () => {},
-              err => expect(err.message).toBe('Could not load books')
-            );
+      use(deps, (service: BookShelf, httpMock: HttpTestingController) => {
 
-          const request = httpMock
-            .expectOne('http://localhost:4730/books');
+        service
+          .all()
+          .subscribe(
+            () => {},
+            err => expect(err.message).toBe('Could not load books')
+          );
 
-          request.error(new ErrorEvent('not found'), { statusText: 'Could not load books'});
-        })));
+        const request = httpMock
+          .expectOne('http://localhost:4730/books');
+
+        request.error(new ErrorEvent('not found'), { statusText: 'Could not load books'});
+        }));
   });
 });
+
+/**
+ * TODO: Not yet ready for reuse
+ * @param deps Dependencies that should be injected by test moduke
+ * @param test Callback providing instantuated dependencies
+ * @returns easy to use function setting up all given test dependencies
+ */
+function use(
+  deps: [typeof BookShelf, typeof HttpTestingController],
+  test: (b: BookShelf, h: HttpTestingController ) => void
+) {
+  return inject(deps, fakeAsync((s, h) => test(s, h)));
+}
